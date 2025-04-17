934937659578210081cea042435c744c6f07755a
badrbin <124630341+badrbin@users.noreply.github.com>
2025-04-18 01:31:05 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='erpnext_custom',
+    version='0.0.1',
+    description='Custom scripts for ERPNext',
+    author='Your Name',
+    author_email='email@example.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
