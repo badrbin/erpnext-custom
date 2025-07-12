ca73132d86328337118df01f6d78d1d692e865f7
badrbin <124630341+badrbin@users.noreply.github.com>
2025-07-12 06:30:36 +0300

    Add files via upload

--- badr_statment/hooks.py
+++ badr_statment/hooks.py
@@ -1,1 +1,6 @@
-# Hooks configuration for badr_statment
+app_name = "badr_statment"
+app_title = "Badr Statement"
+app_publisher = "Your Name or Company"
+app_description = "تقرير كشف حساب العملاء مع إجمالي أوامر البيع والفواتير وسندات التسليم"
+app_email = "you@example.com"
+app_license = "MIT"
