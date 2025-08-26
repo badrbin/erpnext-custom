512b7eb55b12284aff289d433055c9ebcae98457
badrbin <124630341+badrbin@users.noreply.github.com>
2025-08-26 23:35:19 +0300

    Delete badr_statment directory

--- badr_statment/hooks.py
+++
@@ -1,9 +0,0 @@
-app_name = "badr_statment"
-app_title = "Badr Statement"
-app_publisher = "Your Name or Company"
-app_description = "تقرير كشف حساب العملاء مع إجمالي أوامر البيع والفواتير وسندات التسليم"
-app_email = "you@example.com"
-app_license = "MIT"
-
-app_include_js = []
-app_include_css = []
